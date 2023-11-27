668623507c48260ad3a46e42f83edc89bbbaccb7
Kieren James Hussey <khussey45@gmail.com>
2023-11-26 20:10:48 -0500

    changes to story

--- server.js
+++ server.js
@@ -3,14 +3,13 @@ const exphbs = require('express-handlebars');
 const path = require('path');
 const bodyParser = require('body-parser');
 const fs = require('fs');
-const OpenAI = require('openai');
 const axios = require('axios');
-
+const OpenAI = require('openai');
 require('dotenv').config();
 
 const app = express();
-const openai = new OpenAI(process.env.OPENAI_API_KEY);
 const apiUrl = 'https://api.openai.com/v1/engines/davinci/completions';
+const openai = new OpenAI(process.env.OPENAI_API_KEY);
 
 // Set up Handlebars view engine
 app.set('views', path.join(__dirname, "/Views"));
@@ -23,15 +22,12 @@ app.set('view engine', 'hbs');
 
 // Serve static files from the root directory
 app.use(express.static(__dirname));
-
-// Body Parser middleware to handle JSON data
 app.use(bodyParser.json());
 
 // Import and use your main router
 const indexRouter = require('./Routers/index');
 app.use('/', indexRouter);
 
-// POST route for generating speech
 app.post('/generate-speech', async (req, res) => {
     try {
         const mp3 = await openai.audio.speech.create({
@@ -41,7 +37,7 @@ app.post('/generate-speech', async (req, res) => {
         });
 
         const buffer = Buffer.from(await mp3.arrayBuffer());
-        const speechFilePath = path.join(__dirname, 'speech.mp3'); // Save in root directory
+        const speechFilePath = path.join(__dirname, 'speech.mp3');
         await fs.promises.writeFile(speechFilePath, buffer);
 
         res.json({ url: '/speech.mp3' });
@@ -51,29 +47,39 @@ app.post('/generate-speech', async (req, res) => {
     }
 });
 
+// Story generation function using OpenAI
+async function generateStory(prompt) {
+    const headers = {
+        'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
+        'Content-Type': 'application/json'
+    };
+
+    const data = {
+        model: "text-davinci-003", 
+        prompt: prompt,
+        max_tokens: 500
+    };
+
+    try {
+        const response = await axios.post('https://api.openai.com/v1/completions', data, { headers: headers });
+        return response.data.choices[0].text.trim();
+    } catch (error) {
+        console.error('Error generating story:', error);
+        throw error;  // Rethrow the error to be handled by the caller
+    }
+}
+
+// POST route for generating a story
 app.post('/generate-story', async (req, res) => {
     try {
-        const response = await axios.post(apiUrl, {
-            prompt: "Write a short story about programmers discovering aliens", 
-            max_tokens: 150
-        }, {
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${process.env.OPENAI_API_KEY}` // Corrected this line
-            }
-        });
-
-        const generatedText = response.data.choices[0].text;
-        res.json({ story: generatedText });
+        // You can modify the prompt as needed or get it from the request
+        const story = await generateStory("Write a story about a programmer that discovered aliens");
+        res.json({ story: story });
     } catch (error) {
-        console.error('Error generating story:', error.response ? error.response.data : error.message);
+        console.error('Error in /generate-story route:', error);
         res.status(500).send('Error generating story');
     }
 });
-
-
-
-
 
 // Start the server
 const PORT = process.env.PORT || 3000;
